65443c179649e8bb20ac9b66dedb14754f4a8b35
carlos reyes <carlosarp2000@gmail.com>
2022-06-09 16:59:27 +0200

    configuracion proyecto e inicio pagina home

---
+++ src/components/Slider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Slider = () => {
+    return (
+        <div className="SliderContainer">
+            <img src="" alt="" />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, ea.</p>
+        </div>
+    );
+}
+
+export default Slider;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='HomeContainer'>
+            
+            <div className="TextHomeContainer">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit esse, sapiente dicta, fugiat voluptatibus explicabo voluptatem aut iusto porro rerum assumenda ex, enim atque. Velit corrupti, non ullam sit distinctio asperiores quibusdam voluptatem tempora officia voluptas porro amet nobis. Ab!</p>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
