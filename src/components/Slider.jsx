d4ff421ea270f98fce4cba8520cc23976655445d
carlos reyes <carlosarp2000@gmail.com>
2022-06-09 17:08:24 +0200

    aplicar componente slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import { useState } from 'react';
 
-const Slider = () => {
+const Slider = (props) => {
+
+    const images = props.imgs
+    const [selectedIndex, setSelectedIndex] =  useState(0);
+    const [selectedImage, setSelectedImage] = useState(images[0]);
+
+    /**
+     * 
+     * @param {string[]} images this is the array of images that are going to be displayed
+     * @param {boolean} next this is a boolean that indicates if the image is going to be selected next or previous
+     */
+     const selectNewImage = (images, next = true) => {
+        const condition = next ? selectedIndex < images.length - 1 : selectedIndex > 0;
+        const nextIndex = next ? (condition ? selectedIndex + 1 : 0) : (condition ? selectedIndex - 1 : images.length - 1);
+        setSelectedImage(images[nextIndex]);
+        setSelectedIndex(nextIndex)
+     }
+
+    const prevImage = () => selectNewImage(images, false);
+
+    const nextImage = () => selectNewImage(images);
+
     return (
         <div className="SliderContainer">
-            <img src="" alt="" />
+            <img src={require(`../images/${selectedImage}`)} alt={selectedImage} />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, ea.</p>
+            <div className='btnContainer'>
+                <button onClick={prevImage}>{"<"}</button>
+                <button onClick={nextImage}>{">"}</button>
+            </div>
         </div>
     );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Slider from '../components/Slider';
 
 const Home = () => {
+    const images = ['asombred.jpg', '30sbackground.jpg']
+
     return (
         <div className='HomeContainer'>
-            
+            <Slider imgs = {images}/>
             <div className="TextHomeContainer">
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit esse, sapiente dicta, fugiat voluptatibus explicabo voluptatem aut iusto porro rerum assumenda ex, enim atque. Velit corrupti, non ullam sit distinctio asperiores quibusdam voluptatem tempora officia voluptas porro amet nobis. Ab!</p>
             </div>
